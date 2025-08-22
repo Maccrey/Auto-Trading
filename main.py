9fc41d3a08a3a71e601e277efd605313c9ffc905
Maccrey <m01071630214@gmail.com>
2025-08-23 00:35:05 +0900

    거래 로그 및 백업 파일 생성; 거래 상태 및 수익 정보 포함

--- main.py
+++ main.py
@@ -3263,7 +3263,7 @@ def start_dashboard():
     start_tts_worker()
 
     root = tk.Tk()
-    root.title("그리드 투자 자동매매 대시보드 v2.6")
+    root.title("그리드 투자 자동매매 대시보드 v2.7")
     root.geometry("1400x900")
 
     def on_closing():
